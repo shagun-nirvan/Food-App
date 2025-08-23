336254bfe72d5d6ef441bc41d7ad528b20aabba3
[Shagun Nirvan] <[nirvanshagun173@gmail.com]>
2025-08-23 13:30:19 +0530

    new

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5174"
+    // const frontend_url = "http://localhost:5174"
+    
 
     try {
         const newOrder = new orderModel({
@@ -17,6 +18,7 @@ const placeOrder = async (req,res) => {
             amount : req.body.amount,
             address : req.body.address
         })
+        const {origin} = req.headers;
         await newOrder.save();
         await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}})
 
@@ -45,8 +47,8 @@ const placeOrder = async (req,res) => {
         const session = await stripe.checkout.sessions.create({
             line_items : line_items,
             mode: 'payment',
-            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+            success_url:`${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${origin}/verify?success=false&orderId=${newOrder._id}`
         })
 
         res.json({success:true,session_url:session.url})
